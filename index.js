85819af31ad6c6b346512948b16afbf72179c10b
QuocTuan18CLC1 <QuocTuan18CLC1>
2022-10-31 10:17:39 +0700

    add readme

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
+
+mongoose.connect(process.env.APP_MONGO_DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors());
 app.use(express.json());
